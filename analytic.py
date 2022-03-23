354f93410e87d42830467c906dce5ecfde2ded34
Evans.chens <john113188@gmail.com>
2022-03-23 11:07:45 +0800

    version_2_average

--- analytic.py
+++ analytic.py
@@ -6,5 +6,13 @@ with open('reviews.txt', 'r') as f:
         data.append(line)
         if count % 10000 == 0:
             print(count)
+print('total is: ', len(data), 'data')
 
-print(data[2])
+
+sum_len = 0
+for d in data:
+    sum_len += len(d)
+print(sum_len / count)
+
+
+
